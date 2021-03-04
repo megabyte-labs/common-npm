5a6de0dd5b8e698e8484dc31b20c28e32ba068bf
Brian Zalewski <brian@enviedsolutions.com>
2021-03-04 23:23:35 +0000

    Update files/.vscode/extensions.json, files/.vscode/launch.json, files/.vscode/tasks.json, files/bin/cli, files/src/tsconfig.json, files/.gitignore, files/.npmignore, files/.nycrc, files/ava.config.js, files/tsconfig.json, files/tsconfig.module.json, update.sh files Deleted .huskyrc

---
+++ update.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# This script performs maintenance on this repository. It ensures git submodules are
+# installed and then copies over base files from the modules. It also generates the
+# documentation.
+
+set -ex
+
+PROJECT_TYPE=npm
+
+# Retrieve latest version of the submodules
+if [ ! -d "./.modules/docs" ]; then
+  cd ./.modules
+  git submodule add -b master https://gitlab.com/megabyte-space/documentation/$PROJECT_TYPE.git docs
+  cd ..
+else
+  cd ./.modules/docs
+  git checkout master && git pull origin master
+  cd ../..
+fi
+if [ ! -d "./.modules/shared" ]; then
+  cd ./.modules
+  git submodule add -b master https://gitlab.com/megabyte-space/common/shared.git
+  cd ..
+else
+  cd ./.modules/shared
+  git checkout master && git pull origin master
+  cd ../..
+fi
+
+# Copy files from the shared module
+cp -Rf ./.modules/shared/.github .
+cp -Rf ./.modules/shared/.gitlab .
+cp -Rf ./.modules/shared/.vscode .
+cp ./.modules/shared/.cspell.json .cspell.json
+cp ./.modules/shared/.editorconfig .editorconfig
+cp ./.modules/shared/.eslintrc .eslintrc
+cp ./.modules/shared/CODE_OF_CONDUCT.md CODE_OF_CONDUCT.md
+
+# Copy files from the project type's module
+cp -Rf ./.modules/$PROJECT_TYPE/files/ .
+
+# Generate the documentation
+jq -s '.[0] * .[1]' template.json ./.modules/docs/common.json > __bp.json | true
+npx -y @appnest/readme generate --config __bp.json --input ./.modules/docs/blueprint-contributing.md --output CONTRIBUTING.md | true
+npx -y @appnest/readme generate --config __bp.json --input ./.modules/docs/blueprint-readme.md | true
+rm __bp.json
+
+echo "*** Done updating meta files and generating documentation ***"
