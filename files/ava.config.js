dc1e8c25343c8ed95676188977b21789cec0f47d
Gihan S <gihanshp@gmail.com>
2021-04-24 19:47:22 +0530

    Removed deprecate 'sources' configuration for ava.js

--- files/ava.config.js
+++ files/ava.config.js
@@ -2,7 +2,6 @@ export default {
   failFast: true,
   files: ["!diff", "!test", "!build/module/**"],
   ignoredByWatcher: ["diff", "test"],
-  sources: ["build/main/**/*.js"],
   timeout: "60s",
   typescript: {
     rewritePaths: {
