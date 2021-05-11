397ee7d4dc6166538f200e0220aaa062002a7849
Brian Zalewski <brian@megabyte.space>
2021-05-11 01:58:45 +0000

    Update update.sh, .start.sh files

---
+++ .start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# This script performs maintenance on NPM package repositories. It ensures git submodules are
+# installed and then copies over base files from the modules. It also generates the
+# documentation and performs other miscellaneous tasks.
+
+set -e
+
+curl -sL https://git.io/_has | bash -s git jq node npm wget
+export REPO_TYPE=npm
+git submodule update --init --recursive
+if [ ! -f "./.modules/${REPO_TYPE}/update.sh" ]; then
+  mkdir -p ./.modules
+  git submodule add -b master https://gitlab.com/megabyte-space/common/$REPO_TYPE.git ./.modules/$REPO_TYPE
+else
+  cd ./.modules/$REPO_TYPE
+  git checkout master && git pull origin master
+  cd ../..
+fi
+bash ./.modules/$REPO_TYPE/update.sh
--- update.sh
+++ update.sh
@@ -1,63 +1,42 @@
 #!/bin/bash
 
-# This script performs maintenance on this repository. It ensures git submodules are
-# installed and then copies over base files from the modules. It also generates the
-# documentation.
+# This script performs maintenance on NPM package repositories. It ensures git submodules
+# are installed and then copies over base files from the modules. It also generates the
+# documentation and performs other miscellaneous tasks.
 
-set -ex
+set -e
 
-PROJECT_TYPE=npm
-
-# Retrieve latest version of the submodules
-if [ ! -d "./.modules/docs" ]; then
-  cd ./.modules
-  git submodule add -b master https://gitlab.com/megabyte-space/documentation/$PROJECT_TYPE.git docs
-  cd ..
-else
-  cd ./.modules/docs
-  git checkout master && git pull origin master
-  cd ../..
-fi
+# Ensure shared submodule is present
 if [ ! -d "./.modules/shared" ]; then
-  cd ./.modules
-  git submodule add -b master https://gitlab.com/megabyte-space/common/shared.git
-  cd ..
+  mkdir -p ./.modules
+  git submodule add -b master https://gitlab.com/megabyte-space/common/shared.git ./.modules/shared
 else
   cd ./.modules/shared
   git checkout master && git pull origin master
   cd ../..
 fi
 
+source "./.modules/shared/update.lib.sh"
+
+# Install software dependencies if they are missing
+ensure_node_installed
+ensure_jq_installed
+
+# Ensure documentation partials submodule is present and in sync with master
+ensure_project_docs_submodule_latest
+
 # Copy files from the shared module
 cp -Rf ./.modules/shared/.github .
 cp -Rf ./.modules/shared/.gitlab .
 cp -Rf ./.modules/shared/.vscode .
-cp ./.modules/shared/.cspell.json .cspell.json
 cp ./.modules/shared/.editorconfig .editorconfig
 cp ./.modules/shared/.eslintrc .eslintrc
 cp ./.modules/shared/CODE_OF_CONDUCT.md CODE_OF_CONDUCT.md
 
-# Copy files from the project type's module
-cp -Rf ./.modules/$PROJECT_TYPE/files/ .
+# Apply updates from shared files
+copy_project_files_and_generate_package_json
+generate_documentation
+misc_fixes
 
-# Ensure the pre-commit hook is executable
-chmod 755 .husky/pre-commit
-
-# Generate the documentation
-if [ -f ./.blueprint.json ]; then
-  jq -s '.[0] * .[1]' .blueprint.json ./.modules/docs/common.json > __bp.json | true
-  npx -y @appnest/readme generate --config __bp.json --input ./.modules/docs/blueprint-contributing.md --output CONTRIBUTING.md | true
-  npx -y @appnest/readme generate --config __bp.json --input ./.blueprint.md | true
-  rm __bp.json
-  
-  # Remove formatting error
-  sed -i .bak 's/](#-/](#/g' README.md
-  rm README.md.bak | true
-  sed -i .bak 's/](#-/](#/g' CONTRIBUTING.md
-  rm CONTRIBUTING.md.bak | true
-
-  # Ensure .blueprint.json is formatted properly
-  npx prettier --write .blueprint.json
-fi
-
-echo "*** Done updating meta files and generating documentation ***"
+# Ensure .start.sh is the latest version # TODO: Figure out how to make this work
+# cp ./.modules/$REPO_TYPE/.start.sh .start.sh
