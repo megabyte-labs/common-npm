3f4523b0d9151919ffa516e98e9be67fef79d3a1
Brian Zalewski <brian@megabyte.space>
2021-05-11 05:26:16 +0000

    Update update.sh

--- update.sh
+++ update.sh
@@ -30,7 +30,6 @@ cp -Rf ./.modules/shared/.github .
 cp -Rf ./.modules/shared/.gitlab .
 cp -Rf ./.modules/shared/.vscode .
 cp ./.modules/shared/.editorconfig .editorconfig
-cp ./.modules/shared/.eslintrc .eslintrc
 cp ./.modules/shared/CODE_OF_CONDUCT.md CODE_OF_CONDUCT.md
 
 # Apply updates from shared files
