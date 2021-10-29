17227486c6e07a0ba13b92a1edbbf10f8c3fc1de
Brian Zalewski <brian@megabyte.space>
2021-10-29 02:51:38 +0000

    Update files-cli/.vscode/extensions.json, package.cli.json.handlebars, nodemon-jest.json, files-cli/src/app.service.ts, files-cli/src/app.module.ts, files-cli/src/main.ts, files-cli/tsconfig.json, files-cli/tsconfig.build.json Deleted files-cli/package.json, files-library/package.json, files-cli/src/tsconfig.json

---
+++ files-cli/src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { AppService } from './app.service';
+
+async function bootstrap() {
+  const app = await NestFactory.createApplicationContext(AppModule);
+  const appService = app.get(AppService);
+  console.log(appService.getHello());
+  await app.close();
+}
+bootstrap();
