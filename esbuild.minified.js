b89802d969a9a68fe0a96670ba0348af4f1866fa
Brian Zalewski <brian@megabyte.space>
2021-10-31 00:33:25 +0000

    Update package.cli.json.handlebars, files-cli/src/main.ts, files-cli/src/app.service.ts, files-cli/src/app.module.ts, package.app.json.handlebars, esbuild.bundled.js, esbuild.minified.js, files-cli/bin/cli

---
+++ esbuild.minified.js
@@ -0,0 +1,18 @@
+const esbuild = require('esbuild');
+const { nodeExternalsPlugin } = require('esbuild-node-externals');
+
+esbuild.build({
+  entryPoints: ['src/main.ts'],
+  external: [
+    '@nestjs/microservices',
+    '@nestjs/websockets/socket-module',
+    'cache-manager',
+    'class-transformer',
+    'class-validator'
+  ],
+  bundle: true,
+  minify: true,
+  platform: 'node',
+  plugins: [nodeExternalsPlugin()],
+  outfile: 'dist/main.minified.js'
+});
