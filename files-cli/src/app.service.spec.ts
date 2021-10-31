2692294062d2761aa293a553f2d8acf08c8a2bc2
Megabyte Labs Automation <help@megabyte.space>
2021-10-31 10:26:38 +0000

    automation(prettier-fix) Fixed formatting issues with Prettier on GitLab CI.

--- files-cli/src/app.service.spec.ts
+++ files-cli/src/app.service.spec.ts
@@ -1,22 +1,19 @@
-import { TestingModule } from '@nestjs/testing';
-import { CommandTestFactory } from 'nest-commander-testing';
+import { TestingModule } from '@nestjs/testing'
+import { CommandTestFactory } from 'nest-commander-testing'
 
 describe('Task Command', () => {
-  let commandInstance: TestingModule;
+  let commandInstance: TestingModule
 
   beforeAll(async () => {
     commandInstance = await CommandTestFactory.createTestingCommand({
       imports: [AppModule]
-    }).compile();
-  });
+    }).compile()
+  })
 
   it('should call the "run" method', async () => {
-    CommandTestFactory.setAnswers(['echo Hello World!']);
-    const spawnSpy = jest.spyOn(childProcess, 'spawn');
-    await CommandTestFactory.run(commandInstance, ['run']);
-    expect(spawnSpy).toBeCalledWith([
-      'echo Hello World!',
-      { shell: os.userInfo().shell }
-    ]);
-  });
-});
+    CommandTestFactory.setAnswers(['echo Hello World!'])
+    const spawnSpy = jest.spyOn(childProcess, 'spawn')
+    await CommandTestFactory.run(commandInstance, ['run'])
+    expect(spawnSpy).toBeCalledWith(['echo Hello World!', { shell: os.userInfo().shell }])
+  })
+})
