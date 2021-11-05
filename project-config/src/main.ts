0a03f9372c3473853ddeeb4fda3b4002b353d0cc
Brian Zalewski <brian@megabyte.space>
2021-11-05 01:39:11 +0000

    Update project-library/src/main.ts, project-cli/tsconfig.build.json, project-app/tsconfig.build.json, project-config/src/main.ts, project-config/package.hbs.json, project-library/package.hbs.json, project-plugin/package.hbs.json, project-plugin/src/main.ts Deleted .yamllint

---
+++ project-config/src/main.ts
@@ -0,0 +1,15 @@
+/**
+ * This function takes a string and returns the first character.
+ * All of the functions in this package should have comments
+ * in this format.
+ *
+ * @param message - Any string
+ * @returns The first character in the `message` string
+ */
+function myExportedFunction(message: string) {
+  return message.charAt(0)
+}
+
+/**
+ * Add config logic here
+ */
